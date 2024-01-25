3bb0cc0b040ebd047908cf09bbae561228933e0d
42arch <rend42@163.com>
2024-01-25 22:29:29 +0800

    use local fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,36 @@
 import { ReactNode } from 'react'
-import { Inter as FontSans } from 'next/font/google'
+import localFont from 'next/font/local'
 import { cn } from '@/lib/utils'
 import { Toaster } from '@/components/ui/toaster'
 import '@/styles/globals.css'
 import 'mapbox-gl/dist/mapbox-gl.css'
 import '@mapbox/mapbox-gl-draw/dist/mapbox-gl-draw.css'
 
-export const fontSans = FontSans({
-  subsets: ['latin'],
-  variable: '--font-sans'
+export const fontInter = localFont({
+  src: [
+    {
+      path: '../../public/fonts/inter-regular.woff2',
+      weight: '400',
+      style: 'normal'
+    },
+    {
+      path: '../../public/fonts/inter-medium.woff2',
+      weight: '500',
+      style: 'medium'
+    },
+    {
+      path: '../../public/fonts/inter-semibold.woff2',
+      weight: '600',
+      style: 'semibold'
+    },
+    {
+      path: '../../public/fonts/inter-bold.woff2',
+      weight: '700',
+      style: 'bold'
+    }
+  ],
+  variable: '--font-inter',
+  display: 'swap'
 })
 
 export default function RootLayout({ children }: { children: ReactNode }) {
@@ -18,7 +40,7 @@ export default function RootLayout({ children }: { children: ReactNode }) {
       <body
         className={cn(
           'min-h-screen bg-background font-sans antialiased',
-          fontSans.variable
+          fontInter.variable
         )}
       >
         {children}
